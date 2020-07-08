96e3117df57797d03cf87ee46520d2c87f51ad72
Jude Pereira <jude@clevertap.com>
2020-07-08 22:19:16 +0200

    Add JUnit 5.

---
+++ src/test/java/com/clevertap/stormdb/StormDBTest.java
@@ -0,0 +1,48 @@
+package com.clevertap.stormdb;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.clevertap.stormdb.StormDB.KVConsumer;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+
+class StormDBTest {
+
+    @Test
+    void simpleTest() throws IOException {
+        final Path path = Files.createTempDirectory("stormdb");
+
+        final int valueSize = 28;
+        final StormDB db = new StormDB(valueSize, path.toString());
+
+        final int records = 100;
+        for (int i = 0; i < records; i++) {
+            final ByteBuffer value = ByteBuffer.allocate(valueSize);
+            value.putInt((int) (Math.random() * 100000000)); // Insert a random value.
+            db.put(i, value.array());
+
+            value.clear();
+            value.putInt(i); // Insert a predictable value.
+            db.put(i, value.array());
+        }
+
+        // Verify.
+        for (int i = 0; i < records; i++) {
+            final byte[] bytes = db.randomGet(i);
+            final ByteBuffer value = ByteBuffer.wrap(bytes);
+            assertEquals(i, value.getInt());
+        }
+
+        // Iterate sequentially.
+        db.iterate(new KVConsumer() {
+            @Override
+            public void accept(int key, byte[] data, int offset) {
+                final ByteBuffer value = ByteBuffer.wrap(data, offset, valueSize);
+                assertEquals(key, value.getInt());
+            }
+        });
+    }
+}
