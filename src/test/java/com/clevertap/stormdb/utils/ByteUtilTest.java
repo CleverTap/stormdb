c4f596c64aaae7247a5e8411ed1021058f1821e5
Jude Pereira <jude@clevertap.com>
2020-07-13 20:10:22 +0200

    Change to a parametrized test.

--- src/test/java/com/clevertap/stormdb/utils/ByteUtilTest.java
+++ src/test/java/com/clevertap/stormdb/utils/ByteUtilTest.java
@@ -4,21 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.nio.ByteBuffer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * Created by Jude Pereira, at 14:41 on 09/07/2020.
  */
 class ByteUtilTest {
 
-    @Test
-    void toInt() {
-        int[] ints = new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE, 0, -1, 1, 28,
-                Integer.MAX_VALUE / 2};
-        for (int anInt : ints) {
-            final ByteBuffer buf = ByteBuffer.allocate(8);
-            buf.position(2);
-            buf.putInt(anInt);
-            assertEquals(anInt, ByteUtil.toInt(buf.array(), 2));
-        }
+    @ParameterizedTest
+    @ValueSource(ints = {Integer.MIN_VALUE,
+            Integer.MAX_VALUE, 0, -1, 1, 28,
+            Integer.MAX_VALUE / 2})
+    void toInt(final int val) {
+        final ByteBuffer buf = ByteBuffer.allocate(8);
+        buf.position(2);
+        buf.putInt(val);
+        assertEquals(val, ByteUtil.toInt(buf.array(), 2));
     }
 }
